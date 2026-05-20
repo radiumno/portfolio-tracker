bb7b8e0aa16234214a2dfe611f43e5c98684b890
Ra <sunyiradium@gmail.com>
2026-05-20 23:25:51 +0800

    fix: guard provider imports until Tasks 6-7 are implemented

--- data/providers/__init__.py
+++ data/providers/__init__.py
@@ -1,4 +1,11 @@
-from data.providers.akshare_provider import AkshareProvider
-from data.providers.yfinance_provider import YFinanceProvider
+try:
+    from data.providers.akshare_provider import AkshareProvider
+except ImportError:
+    AkshareProvider = None  # type: ignore
+
+try:
+    from data.providers.yfinance_provider import YFinanceProvider
+except ImportError:
+    YFinanceProvider = None  # type: ignore
 
 __all__ = ["AkshareProvider", "YFinanceProvider"]
